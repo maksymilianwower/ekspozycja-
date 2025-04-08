a7dc191d478c8436293c3a8c72c4618cb3ee1b98
maksymilianwower <126483368+maksymilianwower@users.noreply.github.com>
2025-04-08 11:55:38 +0200

    html and css added

--- ekspozycja/app.py
+++ ekspozycja/app.py
@@ -1,21 +1,41 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+from flask import Flask, render_template, request, redirect, url_for
 from config import Config
+import os
+from werkzeug.utils import secure_filename
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate 
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
 db = SQLAlchemy(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+migrate = Migrate(app, db)
+from models import Photo
 
-from models import *
+UPLOAD_FOLDER = os.path.join('static', 'uploads')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# Example route
 @app.route('/')
-def index():
-    return "Hello from sdfghjkjhgfd!"
+def home():
+    return render_template('home.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'photo' not in request.files:
+        return redirect(url_for('home'))
+
+    file = request.files['photo']
+    if file.filename == '':
+        return redirect(url_for('home'))
+
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+    # Save to database
+    new_photo = Photo(filename=filename)
+    db.session.add(new_photo)
+    db.session.commit()
+
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
